613d22e46548ee022cdfa2ded5c32cfba13a4781
George <gnjuki19@gmail.com>
2022-07-23 14:19:58 +0300

    Practiced lists and keys

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,18 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  const x = projects.map(item => {
+    return (
+      <ProjectItem name={item.name} about={item.about} technologies={item.technologies} key={item.id}/>
+    
+    )
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {x}
+      </div>
     </div>
   );
 }
